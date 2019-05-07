aad1ce468e8d2dccf10db8f5c986113589fab9a0
William James Oliver <james@eastsidegamestudio.com>
2019-05-07 16:34:22 -0700

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by James Oliver on 2019-05-07.
+//  Copyright © 2019 James Oliver. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
